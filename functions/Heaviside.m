65cb5eedca3c5ac7fa4a427143014fe603598c4b
snv22002 <sy.nguyen-van@uconn.edu>
2025-11-18 12:07:41 -0500

    .

--- functions/Heaviside.m
+++
@@ -1,7 +0,0 @@
-
-function [rho_H, d_rho_H] = Heaviside(rho,beta,eta)
-rho_H = (tanh(beta*eta)+tanh(beta*(rho-eta)))./(tanh(beta*eta)+tanh(beta*(1-eta)));
-d_rho_H =(beta*(sech(beta*(rho-eta))).^2)./(tanh(beta*eta)+tanh(beta*(1-eta)));
-end
-
-
